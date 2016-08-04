e30f49505e59abc3653128a36ed0b0a1b11b1c51
johnlim <johnlim.dingwei@gmail.com>
2016-08-04 16:12:55 +0800

    feat(visibility/hidden): display element (stamps the dom) only when needed. Demo totally sucks but user should get the point.

---
+++ demo/scripts/app.js
@@ -0,0 +1,11 @@
+(function(document) {
+  'use strict';
+
+  var app = document.querySelector('#app');
+
+  app.cart = [];
+
+  app.addShrimps = function() {
+    app.cart = ['Fire Red Shrimps'];
+  };
+})(document);
